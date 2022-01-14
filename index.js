3e0427c516ef355bf7a3f87dd769d6eb764a3111
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-14 15:21:05 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+
+
+
+function appendCat(name) {
+    return appendCat = [...cats, name];
+}
+
+function prependCat(name) {
+return prependCat = [name, ...cats]
+}
+
+function removeLastCat() {
+ return removeLastCat = [...cats.slice(0,2)];
+}
+function removeFirstCat() {
+   return removeFirstCat = [...cats.slice(1)];
+}
